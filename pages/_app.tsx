d3eea9a9f621feb319b4659bfee0175ba92a32c1
Mohammad Kamalipour <mohammad.kp8093@gmail.com>
2022-12-04 18:52:08 +0330

    HomePage Added and colors are initilized as dark and light mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
     return (
         <Provider store={store}>
             <PersistGate loading={null} persistor={persistor}>
-                <Component {...pageProps} />;
+                <Component {...pageProps} />
             </PersistGate>
         </Provider>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,48 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx}",
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+    ],
 
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    theme: {
+        extend: {
+            colors: {
+                "purple-1": "#202874",
+                "purple-2": "#2A3493",
+                "purple-3": "#3440B2",
+                "purple-4": "#3E4CD1",
+                "purple-5": "#4858EF",
+                "purple-6": "#5F6DF1",
+                "purple-7": "#7682F3",
+                "purple-8": "#8D97F5",
+                "purple-9": "#A4ACF7",
+                body: "#FFFFFF",
+                white: "#FFFFFF",
+                smokeWhite: "#F9F9F9",
+                black: "#212121",
+                success: "#3BD49B",
+                alternative: "#F08960",
+                darkMode: {
+                    "purple-1": "#2C3474",
+                    "purple-2": "#3A4493",
+                    "purple-3": "#4854B2",
+                    "purple-4": "#5664D1",
+                    "purple-5": "#4858EF",
+                    "purple-6": "#7884F4",
+                    "purple-7": "#8B95F6",
+                    "purple-8": "#9EA6F8",
+                    "purple-9": "#B1B7FA",
+                    body: "#212121",
+                    white: "#F9F9F9",
+                    smokeWhite: "#F9F9F9",
+                    black: "#212121",
+                    success: "#3BD49B",
+                    alternative: "#F29F7E",
+                },
+            },
+        },
+    },
+    plugins: [],
 };
