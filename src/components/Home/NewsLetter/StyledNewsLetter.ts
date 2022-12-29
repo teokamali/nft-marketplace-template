37ea84e529de50dbccc3bba546541d97f777a048
MamadK9 <mohammad.kp8093@gmail.com>
2022-12-29 12:48:03 +0330

    newsletter bug fix

--- src/components/Home/NewsLetter/StyledNewsLetter.ts
+++ src/components/Home/NewsLetter/StyledNewsLetter.ts
@@ -5,10 +5,10 @@ export const NewsLetterWrapper = styled.div`
     ${tw`w-full py-10 mt-32 bg-[#E5EAF0] dark:bg-black flex flex-col items-center gap-4`}
 `;
 export const NewsLetterTitle = styled.h5`
-    ${tw`text-[25px] font-black `}
+    ${tw`text-[25px] font-black text-black dark:text-white `}
 `;
 export const NewsLetterDescription = styled.p`
-    ${tw`w-full md:w-[40%] text-center text-base font-normal`}
+    ${tw`w-full md:w-[40%] text-center text-base font-normal text-black dark:text-white`}
 `;
 export const NewsLetterBlueText = styled.span`
     ${tw`text-base font-normal text-purple-5`}
