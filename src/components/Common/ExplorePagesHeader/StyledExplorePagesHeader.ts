3f49c85f6fad91db6818e45dbdcf73dbc2af4c92
MamadK9 <mohammad.kp8093@gmail.com>
2023-01-17 14:17:14 +0330

    explore header style modified

--- src/components/Common/ExplorePagesHeader/StyledExplorePagesHeader.ts
+++ src/components/Common/ExplorePagesHeader/StyledExplorePagesHeader.ts
@@ -9,15 +9,15 @@ export const StyledMainContentWrapper = styled.div`
     ${tw`w-[50%] max-w-[500px] flex flex-col flex-grow items-center gap-2`}
 `;
 export const StyledMainContentTitle = styled.h2`
-    ${tw`text-[31px] font-semibold text-center`}
+    ${tw`text-[31px] font-semibold text-center text-black dark:text-white`}
 `;
 
 export const StyledMainContentDescription = styled.p`
-    ${tw`text-title1 font-light text-justify`}
+    ${tw`text-title1 font-light text-justify text-black dark:text-white`}
 `;
 
 export const StyledNavigationWrapper = styled.div`
-    ${tw`flex flex-col items-center min-w-[130px]`}
+    ${tw`flex flex-col items-center min-w-[130px] text-black dark:text-white`}
 `;
 export const StyledIconWrapper = styled.div`
     ${tw`text-current w-6 h-6`}
